dc87d1f2459912f5ec42e6208e234983df4ab90d
ELsina <EBR.SINA@GMAIL.COM>
2018-01-07 12:38:14 +0330

    third test12:38 PM 07-Jan-18

--- Main.java
+++ Main.java
@@ -8,15 +8,14 @@ public class Main {
         TreeClass  l  = new TreeClass("LEFT child");
         TreeClass  rr  = new TreeClass("RIGHT RIGHT child");
         TreeClass  rl  = new TreeClass("RIGHT LEFT child");
-        TreeClass  lr  = new TreeClass("LEFT RIGHT child");
         TreeClass  ll  = new TreeClass("LEFT LEFT child");
+        TreeClass  lr  = new TreeClass("LEFT RIGHT child");
 
         aClass.addChild(l,r);
         r.addChild(rl,rr);
         l.addChild(ll,lr);
 
-        aClass.leftC().rightC().parentTree().printf();
-
+        aClass.leftC().rightC().printf();
 
 
 
